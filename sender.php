d7e00ba9d0bde14c79087b0d3ecc6f67539f6689
ker4ik <105843969+ker4ik13@users.noreply.github.com>
2023-04-11 22:14:16 +0300

    Update sender.php

--- sender.php
+++ sender.php
@@ -1,15 +1,17 @@
 <?php
+    define ('SITE_DOMAIN', $_SERVER['SERVER_NAME']);
     $name = $_POST['name'];
     $email = $_POST['email'];
 
-	$to = "kirill.mail.tu78@gmail.com"; 
+	$to = "kirill.mail.tu78@gmail.com";
 	$date = date ("d.m.Y"); 
 	$time = date ("h:i");
-	$from = $email;
+	$from = "From: ".SITE_DOMAIN." <info@".SITE_DOMAIN.">\r\n";
 	$subject = "Заявка c сайта";
 
 	$msg="
     Имя: $name /n
     Почта: $email";
-	mail($to, $subject, $msg, "From: $from ");
+	mail($to, $subject, $msg, $from );
+	print_r( $_POST );
 ?>
